45cc22d248b33a981d7096e43f51f88a98dbe133
Nils Lockean <nils@oribi.se>
2021-05-16 15:55:16 +0200

    chore: remove unused javascript

--- docs/index.js
+++ docs/index.js
@@ -1,70 +1,8 @@
-const externalFonts = [] // Array of CSS font-name properties
-const maxFontLoadTime = 2000
-let fontLoadTimeout = null
-
-const checkFont = fontName => {
-  return new Promise(resolve => {
-    let checks = 0
-    let interval = null
-    let hasLoaded = false
-
-    const onSuccess = () => {
-      if (interval !== null) {
-        clearInterval(interval)
-      }
-      document.body.classList.add(
-        fontName.toLowerCase().replaceAll(' ', '-') + '--loaded'
-      )
-      resolve(hasLoaded)
-    }
-
-    const check = () => {
-      checks++
-
-      if (checks >= 20) return onSuccess()
-
-      try {
-        hasLoaded = document.fonts.check('1rem "' + fontName + '"')
-      } catch (_error) {
-        return onSuccess()
-      }
-
-      if (hasLoaded) {
-        onSuccess()
-      }
-    }
-
-    interval = setInterval(check, 100)
-  })
-}
-
-const handleFontsLoaded = () => {
-  clearTimeout(fontLoadTimeout)
-  const allLoaded = externalFonts.every(fontName =>
-    document.fonts.check('1rem "' + fontName + '"')
-  )
-  if (allLoaded) {
-    console.log('All fonts were loaded successfully')
-  } else {
-    console.log(
-      `Stopped waiting for fonts to load after ${
-        maxFontLoadTime / 1000
-      } seconds`
-    )
-  }
-}
-
 const onLoad = () => {
   document.body.classList.remove('no-js')
 
-  if (externalFonts.length) {
-    const fontChecks = externalFonts.map(fontName => checkFont(fontName))
-    fontLoadTimeout = setTimeout(handleFontsLoaded, maxFontLoadTime)
-    Promise.all(fontChecks).then(handleFontsLoaded)
-  }
-
   const currentYearElements = document.querySelectorAll('.current-year')
-  if (!!currentYearElements) {
+  if (currentYearElements) {
     const year = new Date().getFullYear().toString()
     for (const element of currentYearElements) {
       if (element.innerText === year) continue
