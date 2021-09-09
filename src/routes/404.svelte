c1810a9fd97a2a82d78b2bb9a2a35aa7ca6e2511
Nils Lockean <nils@oribi.se>
2021-09-09 21:48:36 +0200

    chore: add static 404 page

---
+++ src/routes/404.svelte
@@ -0,0 +1,23 @@
+<script>
+	import Header from '$lib/header.svelte';
+
+	let visible = true;
+	const handleClick = () => {
+		visible = false;
+	};
+</script>
+
+<svelte:head>
+	<title>Sidan hittades inte</title>
+</svelte:head>
+
+{#if visible}
+	<Header tagline="Sidan hittades inte" transition={false}>
+		<p>
+			Men du kan hitta mer information om våra respektive verksamheter via länkarna på <a
+				on:click={handleClick}
+				href="/">startsidan</a
+			>.
+		</p>
+	</Header>
+{/if}
